834f70150fe76d314c8617507b05982982570316
Ansh Singh <ansh.singh@thejoltapp.com>
2026-05-27 08:42:39 +0530

    initial commit

---
+++ src/test/java/com/ansh/focus/FocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansh.focus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
